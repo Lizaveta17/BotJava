3e3aa6b560aebc7ae8432c1adba75e9adea84b39
elizaveta_ <golyshevaelizaveta@yandex.ru>
2020-10-27 03:13:40 +0500

    add new tests

--- src/test/LogicTest.java
+++ src/test/LogicTest.java
@@ -2,23 +2,67 @@ package test;
 
 import data.Data;
 import data.BaseAnswers;
+import data.BaseCommands;
+import data.AnswerWithKeyboard;
 import logic.Logic;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class LogicTest {
+import java.util.HashSet;
+import java.util.Set;
+
+public class LogicTest extends Logic{
+    Logic logic = new Logic();
 
     @Test
     public void getAnswer() {
-        Logic logic = new Logic();
-        Data data = new Data();
         Long id = (long)1;
-        Assert.assertEquals(BaseAnswers.START, logic.getAnswer(id,"/start"));
-        Assert.assertEquals(BaseAnswers.HELP, logic.getAnswer(id,"Помощь"));
-        Assert.assertEquals(BaseAnswers.HELP, logic.getAnswer(id,"/help"));
-        String test = "Выберите из списка: " + logic.setAsFormatString(Data.getTypes().keySet());
-        Assert.assertEquals(test, logic.getAnswer(id,"Узнать рецепт"));
-        test = "Выберите блюдо: " + logic.setAsFormatString(Data.getTypes().get("Супы").keySet());
-        Assert.assertEquals(test, logic.getAnswer(id,"Супы"));
+        Assert.assertEquals(BaseAnswers.START, getAnswer(id,BaseCommands.START.getTitle()));
+        Assert.assertEquals(BaseAnswers.HELP, getAnswer(id,BaseCommands.HELP_RUS.getTitle()));
+        Assert.assertEquals(BaseAnswers.HELP, getAnswer(id,BaseCommands.HELP_EN.getTitle()));
+        Assert.assertEquals(BaseAnswers.HELP, getAnswer(id,"Привет"));
+
+        idTypes.put(id, "Супы");
+        Assert.assertEquals("Борщ\n\n" + Data.getTypes().get("Супы").get("Борщ"),
+                getAnswer(id,"Борщ"));
+        Assert.assertEquals(BaseAnswers.HELP, getAnswer(id,"Борщ"));
+
+        idIngredients.put(id, "Рыба");
+        Assert.assertEquals("Мимоза\n\n" + Data.getTypes().get("Салаты").get("Мимоза"),
+                getAnswer(id, "Мимоза"));
+    }
+
+    @Test
+    public void getSetForKeyboard() {
+        Long id = (long)2;
+        Assert.assertEquals(Data.getTypes().keySet(), getSetForKeyboard(id, AnswerWithKeyboard.LIST, ""));
+
+        Assert.assertEquals(getSetForKeyboard(id, AnswerWithKeyboard.DISH, "Соусы"),
+                Data.getTypes().get(idTypes.get(id)).keySet());
+
+        Assert.assertEquals(Data.getIngredients().keySet(),
+                getSetForKeyboard(id, AnswerWithKeyboard.INGREDIENT, ""));
+
+
+        Assert.assertEquals(getSetForKeyboard(id, AnswerWithKeyboard.TOUCH, "Орехи"),
+                Data.getIngredients().get(idIngredients.get(id)));
+
+        Set<String> set = new HashSet<>();
+        set.add(BaseCommands.NAME.getTitle());
+        set.add(BaseCommands.INGREDIENT.getTitle());
+        Assert.assertEquals(set, getSetForKeyboard(id, AnswerWithKeyboard.WAY, ""));
+
+        set = new HashSet<>();
+        Assert.assertEquals(set, getSetForKeyboard(id, AnswerWithKeyboard.UNKNOWN, ""));
+    }
+
+    @Test
+    public void getKeyboardAnswer(){
+        Assert.assertEquals(AnswerWithKeyboard.LIST,getKeyboardAnswer("По названию"));
+        Assert.assertEquals(AnswerWithKeyboard.INGREDIENT,getKeyboardAnswer("По ингредиенту"));
+        Assert.assertEquals(AnswerWithKeyboard.WAY,getKeyboardAnswer("Узнать рецепт"));
+        Assert.assertEquals(AnswerWithKeyboard.DISH,getKeyboardAnswer("Напитки"));
+        Assert.assertEquals(AnswerWithKeyboard.TOUCH,getKeyboardAnswer("Свинина"));
+        Assert.assertEquals(AnswerWithKeyboard.UNKNOWN,getKeyboardAnswer("Привет"));
     }
 }
